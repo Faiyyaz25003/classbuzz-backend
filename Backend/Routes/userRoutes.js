0a905d4065996207cdd78e1d8477d34a36c9b020
Faiyyaz25003 <khanwork25003@gmail.com>
2025-10-21 00:41:17 +0530

    dcsdc

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,11 +4,13 @@ import {
     getAllUsers,
     updateUser,
     toggleBlockUser,
+    registerUser,
     loginUser,
 } from "../Controller/userController.js";
 
 const router = express.Router();
 
+router.post("/register", registerUser);
 router.get("/", getAllUsers); // get all users
 router.put("/:id", updateUser); // edit user
 router.patch("/:id/block", toggleBlockUser); // block/unblock user
