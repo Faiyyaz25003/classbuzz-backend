1122ac47475e21c1aa1cfb2f09ad2a2ed3608715
Faiyyaz25003 <khanwork25003@gmail.com>
2025-10-25 14:09:41 +0530

    sc

--- Backend/server.js
+++ Backend/server.js
@@ -1,38 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import userRoutes from "./Routes/userRoutes.js";
-// import leaveRoutes from "./Routes/LeaveRoutes.js";
-// import attendanceRoutes from "./Routes/attendanceRoutes.js";
-// import documentRoutes from "./Routes/documentRoutes.js";
-// import feeRoutes from "./Routes/feeRoutes.js";
-
-
-// dotenv.config();
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use("/api/users", userRoutes);
-// app.use("/api/leave", leaveRoutes);
-// app.use("/api/attendance", attendanceRoutes);
-// app.use("/api/documents", documentRoutes);
-// app.use("/api/fees", feeRoutes);
-
-// // MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch(err => console.error("MongoDB connection error:", err));
-
-// // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 
 
 
@@ -44,7 +9,7 @@ import http from "http";
 import initializeWebSocket from "./socket/index.js";
 
 // Routes
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
 import messageRoutes from "./Routes/messageRoutes.js";
 import leaveRoutes from "./routes/LeaveRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
