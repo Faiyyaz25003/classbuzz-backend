8fae7e9ffa5b14c0b291fc8a6a6edec9c0f583e9
Faiyyaz25003 <khanwork25003@gmail.com>
2025-11-02 03:51:55 +0530

    wsx

--- Backend/Models/documentModel.js
+++ Backend/Models/documentModel.js
@@ -1,13 +1,3 @@
-// import mongoose from "mongoose";
-
-// const documentSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   aadhaar: String,
-//   marksheet: String,
-//   photo: String,
-// });
-
-// export default mongoose.model("Document", documentSchema);
 
 
 
