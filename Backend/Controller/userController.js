43d95d819b1e3bccea0a7bd58d90ce4b686acc9c
Faiyyaz25003 <khanwork25003@gmail.com>
2025-11-11 19:56:14 +0530

    user

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -130,37 +130,6 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-// 🔹 User login
-// export const loginUser = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(404).json({ message: "User not found" });
-
-//     // ✅ Check if blocked
-//     if (user.isBlocked) {
-//       return res.status(403).json({ message: "Your account has been blocked by admin" });
-//     }
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch)
-//       return res.status(401).json({ message: "Invalid email or password" });
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Login successful",
-//       user: {
-//         id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         positions: user.positions,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: "Login failed", error });
-//   }
-// };
 
 
 // 🔹 User login with JWT token generation
