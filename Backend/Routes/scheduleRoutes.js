91ecc619d7bd971e7cb6d415171bcf4824b29fa8
Faiyyaz25003 <khanwork25003@gmail.com>
2025-11-27 21:46:29 +0530

    schedule

--- Backend/Routes/scheduleRoutes.js
+++ Backend/Routes/scheduleRoutes.js
@@ -1,12 +1,4 @@
-// import express from "express";
-// import { saveSchedule, getSchedule, getAllSchedules } from "../Controller/scheduleController.js";
-// const router = express.Router();
 
-// router.get("/", getAllSchedules);                            // All Schedules
-// router.get("/:courseId/:semester", getSchedule);             // Get specific schedule
-// router.post("/", saveSchedule);                              // Save / Update timetable
-
-// export default router;
 
 import express from "express";
 import { addSchedule, getAllSchedules, getScheduleById } from "../Controller/scheduleController.js";
